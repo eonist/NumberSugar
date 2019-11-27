77ec9aa08f3180cdd07e4ecc55c679f3ce27b91e
Eonist <30n1st@gmail.com>
2019-11-27 11:37:22 +0100

    Simplify

--- Package.swift
+++ Package.swift
@@ -20,9 +20,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "NumberSugar",
-            dependencies: []),
-        .testTarget(
-            name: "NumberSugarTests",
-            dependencies: ["NumberSugar"]),
+            dependencies: [])
     ]
 )
