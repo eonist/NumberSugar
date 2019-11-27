bfa62cc1b0500e140230cd0b576caac71d898150
Eonist <30n1st@gmail.com>
2019-11-27 11:03:43 +0100

    Add tests

--- NumberSugarTest/NumberSugarTest.swift
+++ NumberSugarTest/NumberSugarTest.swift
@@ -1,11 +1,3 @@
-//
-//  NumberSugarTest.swift
-//  NumberSugarTest
-//
-//  Created by Eon on 27/11/2019.
-//  Copyright © 2019 FutureLab. All rights reserved.
-//
-
 import XCTest
 
 class NumberSugarTest: XCTestCase {
@@ -21,6 +13,9 @@ class NumberSugarTest: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+      XCTAssertEqual(NumberParser.minMax(100, 200, 400), 200)
+      XCTAssertEqual(NumberParser.minMax(500, 200, 400), 400)
+      XCTAssertEqual(NumberParser.minMax(300, 200, 400), 300)
     }
 
     func testPerformanceExample() {
