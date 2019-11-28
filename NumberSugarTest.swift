76b23b47c33ad6b9c1c50aaa8d86e67ce5241a47
Eonist <30n1st@gmail.com>
2019-11-28 15:05:30 +0100

    Update NumberSugarTest.swift

--- NumberSugarTest.swift
+++ NumberSugarTest.swift
@@ -1,15 +1,12 @@
 import XCTest
 
 class NumberSugarTest: XCTestCase {
-
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -17,12 +14,10 @@ class NumberSugarTest: XCTestCase {
       XCTAssertEqual(NumberParser.minMax(500, 200, 400), 400)
       XCTAssertEqual(NumberParser.minMax(300, 200, 400), 300)
     }
-
     func testPerformanceExample() {
         // This is an example of a performance test case.
         measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
