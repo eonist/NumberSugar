7b1b3e5524ebc73eb58697a5033ab47414eebe6f
Eonist <30n1st@gmail.com>
2019-12-05 14:02:31 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "NumberSugar",
-            targets: ["NumberSugar"]),
+            targets: ["NumberSugar"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
